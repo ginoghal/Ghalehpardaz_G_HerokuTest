cac8ffaddaeb4ea7272fc6151a22ee0c3108c50a
ginoghal <46694884+ginoghal@users.noreply.github.com>
2019-10-10 15:39:46 -0400

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,16 @@
 (() => {
     // test fired
     console.log('fired!');
+
+    function myFunction() {
+      var x = document.getElementById("mainNav");
+      if (x.className === "mainNav") {
+        x.className += " responsive";
+      } else {
+        x.className = "mainNav";
+      }
+    }
+
+
+
 })()
