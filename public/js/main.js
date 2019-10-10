9bfd0a1b301213d2a3e4afdd8716fe4b926a9356
ginoghal <46694884+ginoghal@users.noreply.github.com>
2019-10-10 16:36:16 -0400

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -2,15 +2,14 @@
     // test fired
     console.log('fired!');
 
-    function myFunction() {
-      var x = document.getElementById("mainNav");
-      if (x.className === "mainNav") {
-        x.className += " responsive";
-      } else {
-        x.className = "mainNav";
-      }
-    }
+    function hamburgerMenu() {
+  		mainNav.classList.toggle("slideToggle");
+  		button.classList.toggle("expanded");
+  	}
 
+  	button.addEventListener("click", menu, false);
+
+  })();
 
 
 })()
